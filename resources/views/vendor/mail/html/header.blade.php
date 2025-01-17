245f4e3c816b808b6a751329faea8fa423415b4b
Sanni David <sannidavidsmart@gmail.com>
2025-01-17 13:06:48 +0100

    fix: mail logo

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -5,7 +5,7 @@
 <img src="" class="logo" alt="Laravel Logo">
 @else
 <a href="{{ url('/') }}" style="text-decoration: none">
-    <img src="https://cdn.prod.website-files.com/60f88e4b0ddb8e1d95163233/60f89753ce8105038912ef68_brookstone_logoFULL_2021.svg" style="width: 150px" alt="">
+    <img src="https://brookstonecapitals.com/asset/logo.png" style="width: 150px" alt="">
 </a>
 @endif
 </a>
