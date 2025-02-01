008ef5d044a47319a3030eecc2a1981de88dfdde
Sanni David <sannidavidsmart@gmail.com>
2025-02-01 03:41:04 +0100

    feat: fix user err

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -140,7 +140,7 @@ class HomeController extends Controller
 
             $currentQuantityCrypto = $stock->quantity;
 
-            $totalInvestmentCrypto += $investmentAmount;
+            $totalInvestmentCrypto += $investmentAmountCrypto;
             $totalCurrentValueCrypto += $currentValue;
             $totalQuantityCrypto += $currentQuantity;
         }
